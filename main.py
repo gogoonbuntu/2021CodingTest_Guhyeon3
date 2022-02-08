4cec73264a2e86aebc6fb543b3d16ccfca40c5b0
Gogoonbuntu <34636395+gogoonbuntu@users.noreply.github.com>
2022-02-08 12:57:01 +0000

    성공

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # 문자열(숫자포함) 입력받으면, 알파벳은 순서정렬, 숫자는 다 더해서 마지막에 출력
 # 마지막 부분을 간략하게 표현할 수 있을듯
-# 내 코드의 오류 : 숫자가 없을 때 0이 붙음.
-
+# isalpha 함수 사용하면 됨.
 num = 0
 def func(i):
 	global num
@@ -16,6 +15,6 @@ a = [i for i in list(map(func,sorted(input()))) if i]
 s = ''
 for i in a:
 	s+=i
-s += str(num)
+if(num>0) : s+=str(num)
 
 print(s)
